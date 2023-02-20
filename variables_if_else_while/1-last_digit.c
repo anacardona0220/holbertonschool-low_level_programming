bb60c069c06fe510630ce7b39c900dbef1f59ad1
anacardona0220 <anamariacb0220@gmail.com>
2023-02-20 13:14:56 -0800

    1-last_digit.c code

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -17,6 +17,6 @@ printf("Last digit of %d is %d and is greater than 5\n", n, num);
 else if (num == 0)
 printf("Last digit of %d is %d and is 0\n", n, num);
 else
-printf("last digit of %d is %d and is less than 6 and not 0\n", n, num);
+printf("last digit of %d is %d and is 6<0 && num !=0\n", n, num);
 return (0);
 }
