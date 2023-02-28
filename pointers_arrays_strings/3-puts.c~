4b58bd593640b3c65b6d339498baa5b190646728
anacardona0220 <anamariacb0220@gmail.com>
2023-02-28 08:33:03 -0800

    5-rev_string.c code

---
+++ pointers_arrays_strings/3-puts.c~
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - a function that prints a string, followed by a new line
+ *
+ * @str: This is char
+ *
+ */
+void _puts(char *str)
+{
+  int k;
+
+  for (k = 0; str[k] != '\0'; k++)
+  {
+    _putchar(str[k]);
+  }
+  _putchar('\n');
+}
---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * rev_string - a function that reverses a string
+ *
+ * @s: This is char
+ *
+ */
+void rev_string(char *s)
+{
+int k = 0;
+int len = 0;
+char aux;
+while (s[k++])
+len++;
+for (k = len - 1; k >= len / 2; k--)
+{
+aux = s[k];
+s[k] = s[len - k - 1];
+s[len - k - 1] = aux;
+}
+}
