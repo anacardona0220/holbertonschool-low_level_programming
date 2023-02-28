a348efc1c5e535e7366cc6062eab53b8827ac6fd
anacardona0220 <anamariacb0220@gmail.com>
2023-02-28 08:37:19 -0800

    6-puts2.c code

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - a function that prints every other character of a string
+* @str: This is char
+ *
+ */
+void puts2(char *str)
+{
+int k;
+for (k = 0; str[k] != '\0'; k++)
+{
+if (k == 0 || k % 2 == 0)
+{
+_putchar(str[k]);
+}
+}
+_putchar('\n');
+}
