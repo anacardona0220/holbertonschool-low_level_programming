1e277cdd0c5f219b128cf3756487c5c7b3680746
anacardona0220 <124609083+anacardona0220@users.noreply.github.com>
2023-03-03 15:25:22 -0500

    Update 4-rev_array.c

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -9,15 +9,15 @@ nclude "main.h"
 **/
 void reverse_array(int *a, int n)
 {
-	int i, last;
-	last = n - 1;
-	for (i = 0; i < n / 2; i++)
-	{
-		int start, end;
-		start = a[i];
-		end = a[last];
-		a[i] = end;
-		a[last] = start;
-		last--;
-	}
+int i, last;
+last = n - 1;
+for (i = 0; i < n / 2; i++)
+{
+int start, end;
+start = a[i];
+end = a[last];
+a[i] = end;
+a[last] = start;
+last--;
+}
 }
