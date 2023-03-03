6215f780d88f4a1bcc3462c206ac0032d151f904
anacardona0220 <124609083+anacardona0220@users.noreply.github.com>
2023-03-03 15:41:45 -0500

    Update 6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -5,31 +5,31 @@
  * * @cadena: This is the input string
  * *
  * * Return: String capitalized
- * */
+**/
 char *cap_string(char *cadena)
 {
-	int i = 0;
-	while (cadena[i])
-	{
-		while (!(cadena[i] >= 'a' && cadena[i] <= 'z'))
-			i++;
-		if (
-				cadena[i - 1] == ' ' ||
-				cadena[i - 1] == '\t' ||
-				cadena[i - 1] == '\n' ||
-				cadena[i - 1] == ',' ||
-				cadena[i - 1] == '.' ||
-				cadena[i - 1] == ';' ||
-				cadena[i - 1] == '!' ||
-				cadena[i - 1] == '?' ||
-				cadena[i - 1] == '"' ||
-				cadena[i - 1] == '(' ||
-				cadena[i - 1] == ')' ||
-				cadena[i - 1] == '{' ||
-				cadena[i - 1] == '}' ||
-				i == 0)
-			cadena[i] -= 32;
-		i++;
-	}
-	return (cadena);
+int i = 0;
+while (cadena[i])
+{
+while (!(cadena[i] >= 'a' && cadena[i] <= 'z'))
+i++;
+if (
+cadena[i - 1] == ' ' ||
+cadena[i - 1] == '\t' ||
+cadena[i - 1] == '\n' ||
+cadena[i - 1] == ',' ||
+cadena[i - 1] == '.' ||
+cadena[i - 1] == ';' ||
+cadena[i - 1] == '!' ||
+cadena[i - 1] == '?' ||
+cadena[i - 1] == '"' ||
+cadena[i - 1] == '(' ||
+cadena[i - 1] == ')' ||
+cadena[i - 1] == '{' ||
+cadena[i - 1] == '}' ||
+i == 0)
+cadena[i] -= 32;
+i++;
+}
+return (cadena);
 }
