896eeae4c49e9bcd9077b9134c3cf931dc67457c
anacardona0220 <124609083+anacardona0220@users.noreply.github.com>
2023-03-03 18:01:54 -0500

    Update 1-memcpy.c

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,19 +1,19 @@
 #include "main.h"
 /**
  *  * _memcpy - fills n bytes of memory
- *   *
- *    * @dest: pointer to start of memory area
- *     * @src: constant byte to fill with
- *      * @n: num bytes to fill in
- *       *
- *        * Return: pointer to beginning of memory area best
- *         */
+ *  *
+ *  * @dest: pointer to start of memory area
+ *  * @src: constant byte to fill with
+ *  * @n: num bytes to fill in
+ *  *
+ *  * Return: pointer to beginning of memory area best
+**/
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i = 0;
-	for (i = 0; i < n; i++)
-	{
-		dest[i] = src[i];
-	}
-	return (dest);
+unsigned int i = 0;
+for (i = 0; i < n; i++)
+{
+dest[i] = src[i];
+}
+return (dest);
 }
