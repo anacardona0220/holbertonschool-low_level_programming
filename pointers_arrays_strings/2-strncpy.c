87e0c4c3e70bdbc5a831bf504f480f3c66735153
anacardona0220 <124609083+anacardona0220@users.noreply.github.com>
2023-03-03 14:41:03 -0500

    Update 2-strncpy.c

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -9,14 +9,14 @@
 **/
 char *_strncpy(char *dest, char *src, int n)
 {
-	int index;
+int index;
 for (index = 0; index < n && src[index] != '\0'; index++)
-	{
-		dest[index] = src[index];
-	}
-	for (; index  < n; index++)
-	{
-		dest[index] = '\0';
-	}
-	return (dest);
+{
+dest[index] = src[index];
+}
+for (; index  < n; index++)
+{
+dest[index] = '\0';
+}
+return (dest);
 }
