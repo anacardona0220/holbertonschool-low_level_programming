bacc40a0f6dde97178059ffdc6b99f9aa146ad2a
anacardona0220 <124609083+anacardona0220@users.noreply.github.com>
2023-03-03 18:24:00 -0500

    Update 3-strspn.c

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -6,23 +6,23 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-  int i, k, existe;
-  int count = 0;
-  for (i = 0; s[i] != '\0'; i++)
-    {
-      existe = 0;
-      for (k = 0; accept[k] != '\0'; k++)
-	{
-	  if (accept[k] == s[i])
-	    {
-	      count++;
-	      existe = 1;
-	    }
-	}
-      if (existe == 0)
-	{
-	  return (count);
-	}
-    }
-  return (count);
+int i, k, existe;
+int count = 0;
+for (i = 0; s[i] != '\0'; i++)
+{
+existe = 0;
+for (k = 0; accept[k] != '\0'; k++)
+{
+if (accept[k] == s[i])
+{
+count++;
+existe = 1;
+}
+}
+if (existe == 0)
+{
+return (count);
+}
+}
+return (count);
 }
