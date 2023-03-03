1d93a014681092fa464b716fd6d4d6fa2aecf38b
anacardona0220 <anamariacb0220@gmail.com>
2023-03-03 12:39:34 -0800

    6-cap_string.c

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+nclude "main.h"
+/**
+ * * string_toupper - Write a function that changes all lowercase
+ * * string to uppercase.
+ * *
+ * * @cadena: This is the input string
+ * *
+ * * Return: Lowercase changed to uppercase of the string
+ * */
+char *string_toupper(char *cadena)
+{
+	int i;
+	for (i = 0; cadena[i] != '\0'; i++)
+	{
+		if (cadena[i] >= 'a' && cadena[i] <= 'z')
+			cadena[i] = (int)cadena[i] - 32;
+	}
+	return (cadena);
+}
---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * * cap_string - Write a function that capitalizes all words of a string.
+ * *
+ * * @cadena: This is the input string
+ * *
+ * * Return: String capitalized
+ * */
+char *cap_string(char *cadena)
+{
+	int i = 0;
+	while (cadena[i])
+	{
+		while (!(cadena[i] >= 'a' && cadena[i] <= 'z'))
+			i++;
+		if (
+				cadena[i - 1] == ' ' ||
+				cadena[i - 1] == '\t' ||
+				cadena[i - 1] == '\n' ||
+				cadena[i - 1] == ',' ||
+				cadena[i - 1] == '.' ||
+				cadena[i - 1] == ';' ||
+				cadena[i - 1] == '!' ||
+				cadena[i - 1] == '?' ||
+				cadena[i - 1] == '"' ||
+				cadena[i - 1] == '(' ||
+				cadena[i - 1] == ')' ||
+				cadena[i - 1] == '{' ||
+				cadena[i - 1] == '}' ||
+				i == 0)
+			cadena[i] -= 32;
+		i++;
+	}
+	return (cadena);
+}
