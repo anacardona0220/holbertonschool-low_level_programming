6dd202948ea8c4cfee22eee819c07c708acebf92
anacardona0220 <124609083+anacardona0220@users.noreply.github.com>
2023-03-03 16:15:57 -0500

    Update 5-string_toupper.c

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -9,11 +9,11 @@
 **/
 char *string_toupper(char *cadena)
 {
-	int i;
-	for (i = 0; cadena[i] != '\0'; i++)
-	{
-		if (cadena[i] >= 'a' && cadena[i] <= 'z')
-			cadena[i] = (int)cadena[i] - 32;
-	}
-	return (cadena);
+int i;
+for (i = 0; cadena[i] != '\0'; i++)
+{
+if (cadena[i] >= 'a' && cadena[i] <= 'z')
+cadena[i] = (int)cadena[i] - 32;
+}
+return (cadena);
 }
