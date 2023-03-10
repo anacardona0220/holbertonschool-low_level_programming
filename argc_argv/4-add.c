fe56915451a738ed19cc038283f1e361c1a4febc
anacardona0220 <124609083+anacardona0220@users.noreply.github.com>
2023-03-10 11:48:12 -0500

    Update 4-add.c

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -7,7 +7,7 @@
  * @str: array str
  *
  * Return: Always 0 (Success)
- */
+*/
 int check_num(char *str)
 {
 /Declaring variables/
@@ -29,7 +29,7 @@ return (1);
  * @argv: Arguments
  *
  * Return: Always 0 (Success)
- */
+*/
 int main(int argc, char *argv[])
 {
 /Declaring variables/
