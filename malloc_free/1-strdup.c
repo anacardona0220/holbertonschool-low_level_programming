b0d5b74380827243ed4a1511aaf7033760ca184c
anacardona0220 <124609083+anacardona0220@users.noreply.github.com>
2023-03-13 15:47:34 -0500

    Update 1-strdup.c

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "main.h"
 /**
- * _strdup - duplicate to new memory space location
- * @str: char
- * Return: 0
- */
+* _strdup - duplicate to new memory space location
+* @str: char
+* Return: 0
+*/
 char *_strdup(char *str)
 {
 char *newCadena;
 int i, r = 0;
-int size = strlen(str);
-printf("el numero de caracteres es %d\n", size);
 if (str == NULL)
 return (NULL);
-while (str[i] < size)
+i = 0;
+while (str[i] != '\0')
 i++;
 newCadena = malloc(sizeof(char) * (i + 1));
 if (newCadena == NULL)
