fd61a8a1d692949e0fe19daefb22db84d514b9da
anacardona0220 <anamariacb0220@gmail.com>
2023-03-13 15:34:49 -0500

    returns a pointer to newly allocated apace in memory

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ * _strdup - duplicate to new memory space location
+ * @str: char
+ * Return: 0
+ */
+char *_strdup(char *str)
+{
+char *newCadena;
+int i, r = 0;
+int size = strlen(str);
+printf("el numero de caracteres es %d\n", size);
+if (str == NULL)
+return (NULL);
+while (str[i] < size)
+i++;
+newCadena = malloc(sizeof(char) * (i + 1));
+if (newCadena == NULL)
+return (NULL);
+for (r = 0; str[r]; r++)
+newCadena[r] = str[r];
+return (newCadena);
+}
