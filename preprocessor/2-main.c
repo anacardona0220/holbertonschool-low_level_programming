77712a4cde887717e83416f3216d76238a909fb0
anacardona0220 <anamariacb0220@gmail.com>
2023-03-15 14:55:58 -0500

    starting with preprocess

---
+++ preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - a program that prints the name of the file it was compiled
+ * Return: 0
+ */
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
