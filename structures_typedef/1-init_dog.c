8c870bcc071acc79f3b5fa9bfe0311aae928bfc0
anacardona0220 <anamariacb0220@gmail.com>
2023-03-21 12:41:29 -0500

    function that initialize a variable

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+*init_dog - Write a function that initialize a variable of type struct
+*@d: struct
+*@name: name  char *
+*@age: type = float
+*@owner: type = char *
+* Return: My name is Poppy, and I am 3.5 :) - Woof!
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if(d != NULL)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
+}
