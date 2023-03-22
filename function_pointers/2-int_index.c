12523f5c9fec5e883393b8c36d177d6434417492
anacardona0220 <124609083+anacardona0220@users.noreply.github.com>
2023-03-22 14:21:58 -0500

    Update 2-int_index.c

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -9,20 +9,17 @@
 */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-    int i;
-
-    if (array == NULL || cmp == NULL)
-    {
-        return (-1);
-    }
-
-    for (i = 0; i < size; i++)
-    {
-        if (cmp(array[i]))
-        {
-            return (i);
-        }
-    }
-    return (-1);
-
+int i;
+if (array == NULL || cmp == NULL)
+{
+return (-1);
+}
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]))
+{
+return (i);
+}
+}
+return (-1);
 }
