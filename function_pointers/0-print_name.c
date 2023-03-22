f4411157dead981c71fa30a478822ffba9c1eeab
anacardona0220 <anamariacb0220@gmail.com>
2023-03-22 11:38:37 -0500

    start function_pointers

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ *print_name - function that prints a name
+ *@d: struct
+ *@name: name  char *
+ *@f: function
+ * Return: My name is Poppy, and I am 3.5 :) - Woof!
+ */
+void print_name(char *name, void (*f)(char *))
+{
+(*f)(name);
+}
